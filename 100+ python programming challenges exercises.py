323bcf1b4e29aef4128159ff860cfff406d22937
sajid507 <sajid92500@gmail.com>
2020-08-18 21:39:25 +0500

    define add function

---
+++ 100+ python programming challenges exercises.py
@@ -0,0 +1,8 @@
+Python 3.8.4rc1 (tags/v3.8.4rc1:6c38841, Jun 30 2020, 15:17:30) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def add(*x):
+	return sum(x)
+
+>>> add(4,5,6)
+15
+>>> 
