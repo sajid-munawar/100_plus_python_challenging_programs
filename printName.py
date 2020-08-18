57efff909da3695551cf65a2f070485e7cda58e7
sajid507 <sajid92500@gmail.com>
2020-08-18 21:50:29 +0500

    printName

---
+++ printName.py
@@ -0,0 +1,5 @@
+Python 3.8.4rc1 (tags/v3.8.4rc1:6c38841, Jun 30 2020, 15:17:30) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("sajid")
+sajid
+>>> 
